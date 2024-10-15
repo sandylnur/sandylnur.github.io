1a23cabf713b3e4d422e07df90b11850f1747f31
sandyl nursigadoo <sandylnursigadoo@outlook.com>
2024-10-15 23:06:20 +0400

    feat: added hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import m from "mithril";
+import profilePicture from "../assets/images/profile-picture.jpg";
+import githubIcon from "../assets/images/icons/github-icon.svg";
+import linkedinIcon from "../assets/images/icons/linkedin-icon.svg";
 
 function Header() {
     return {
@@ -7,49 +10,63 @@ function Header() {
             const isActive = (route) => currentRoute === route;
 
             return (
-                <header>
-                    <img src="" alt="" />
-                    <nav>
-                        <m.route.Link
-                            href="/"
-                            class={`nav-links ${isActive("/") ? "nav-links-active" : ""} ${
-                                isActive("/") ? "nav-links-no-hover" : ""
-                            }`}
-                        >
-                            Home
-                        </m.route.Link>
-                        <m.route.Link
-                            href="/projects"
-                            class={`nav-links ${isActive("/projects") ? "nav-links-active" : ""} ${
-                                isActive("/projects") ? "nav-links-no-hover" : ""
-                            }`}
-                        >
-                            Projects
-                        </m.route.Link>
-                        <m.route.Link
-                            href="/blog"
-                            class={`nav-links ${isActive("/blog") ? "nav-links-active" : ""} ${
-                                isActive("/blog") ? "nav-links-no-hover" : ""
-                            }`}
-                        >
-                            Blog
-                        </m.route.Link>
-                        <m.route.Link
-                            href="/contact-me"
-                            class={`nav-links ${isActive("/contact-me") ? "nav-links-active" : ""} ${
-                                isActive("/contact-me") ? "nav-links-no-hover" : ""
-                            }`}
-                        >
-                            Contact Me
-                        </m.route.Link>
-                    </nav>
-                    <div class="social-links">
-                        <a href="" target="_blank" rel="noopener noreferrer" aria-label="Github">
-                            <img src="" alt="" />
-                        </a>
-                        <a href="" target="_blank" rel="noopener noreferrer" aria-label="Linkedin">
-                            <img src="" alt="" />
-                        </a>
+                <header class="nav-header">
+                    <m.route.Link href="/" class="nav-logo">
+                        <img class="profile-picture nav-left" src={profilePicture} alt="Sandyl Nursigadoo" />
+                    </m.route.Link>
+                    <div class="nav-right">
+                        <nav class="nav-menu">
+                            <m.route.Link
+                                href="/"
+                                class={`nav-links ${isActive("/") ? "nav-links-active" : ""} ${
+                                    isActive("/") ? "nav-links-no-hover" : ""
+                                }`}
+                            >
+                                Home
+                            </m.route.Link>
+                            <m.route.Link
+                                href="/projects"
+                                class={`nav-links ${isActive("/projects") ? "nav-links-active" : ""} ${
+                                    isActive("/projects") ? "nav-links-no-hover" : ""
+                                }`}
+                            >
+                                Projects
+                            </m.route.Link>
+                            <m.route.Link
+                                href="/blog"
+                                class={`nav-links ${isActive("/blog") ? "nav-links-active" : ""} ${
+                                    isActive("/blog") ? "nav-links-no-hover" : ""
+                                }`}
+                            >
+                                Blog
+                            </m.route.Link>
+                            <m.route.Link
+                                href="/contact-me"
+                                class={`nav-links ${isActive("/contact-me") ? "nav-links-active" : ""} ${
+                                    isActive("/contact-me") ? "nav-links-no-hover" : ""
+                                }`}
+                            >
+                                Contact Me
+                            </m.route.Link>
+                        </nav>
+                        <div class="social-links">
+                            <a
+                                href="https://github.com/sandylnur"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label="Github"
+                            >
+                                <img src={githubIcon} alt="Github" />
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/sandylnursigadoo"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                aria-label="Linkedin"
+                            >
+                                <img src={linkedinIcon} alt="Linkedin" />
+                            </a>
+                        </div>
                     </div>
                 </header>
             );
